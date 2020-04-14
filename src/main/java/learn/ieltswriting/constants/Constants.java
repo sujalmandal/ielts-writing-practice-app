e4bc38efd04ad9e4fe31f87be5a0ccaa089c28b2
Sujal <ss6sujal@gmail.com>
2020-04-14 21:31:27 +0530

    logo.png file name change

--- src/main/java/learn/ieltswriting/constants/Constants.java
+++ src/main/java/learn/ieltswriting/constants/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
 	public static final String FXML = ".fxml";
 	public static final String TASK_INPUT_VIEW = "task-input-view";
 	public static final String PRACTICE_VIEW = "practise-view";
-	public static final String ICON = "image/LOGO.PNG";
+	public static final String ICON = "image/logo.png";
 	public static final String APP_VERSION = "v1.0";
 	public static final String APP_NAME = "IELTS WRITING PRACTICE APP";
 	public static final String GOOGLE_SEARCH_DEFINE_WORD = "http://www.google.com/search?q=define%3A";
